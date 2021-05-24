866e2088446a77ed6ead08ff447df5e3cb7610a5
Chad Smith <smith.chad12@gmail.com>
2021-05-23 23:36:13 -0500

    Initial Commit

---
+++ MoonShot/MoonShotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonShotApp.swift
+//  MoonShot
+//
+//  Created by Chad Smith on 5/23/21.
+//
+
+import SwiftUI
+
+@main
+struct MoonShotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
